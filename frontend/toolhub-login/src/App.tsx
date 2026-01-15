adca3d2576cfd21dae90be6a258d53550706b629
Anikesh Thakur <anikeshthakur85@gmail.com>
2026-01-15 23:42:39 +0530

    feat(frontend): change in clerk key

--- frontend/toolhub-login/src/App.tsx
+++ frontend/toolhub-login/src/App.tsx
@@ -1,8 +1,7 @@
 import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/clerk-react";
 import RedirectBack from "./RedirectBack.tsx";
 
-const CLERK_PUBLISHABLE_KEY =
-  "pk_test_ZWFzeS1tYXJ0aW4tMjUuY2xlcmsuYWNjb3VudHMuZGV2JA";
+const CLERK_PUBLISHABLE_KEY = "pk_live_Y2xlcmsuaG9zdGluZ2Zyb21wdXJ2YS54eXok";
 
 export default function App() {
   return (
